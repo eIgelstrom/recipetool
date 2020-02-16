eefd60e1584bfc70cd188be7c5342daa21773024
eIgelstrom <emmaig@kth.se>
2020-02-16 18:01:09 +0100

    code fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ const App = () => {
     getRecepies();
   }, [query]);
 
-  
   const getRecepies = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
